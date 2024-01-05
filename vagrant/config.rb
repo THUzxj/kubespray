8e7d5148a79d75650f9c6145522c989801b7c87d
Xingjian Zhang <xingjian.zhang@qq.com>
2024-01-05 12:24:59 +0000

    Add my cluter config

---
+++ vagrant/config.rb
@@ -0,0 +1,8 @@
+# $os="centos"
+$inventory = "inventory/mycluster"
+# $ansible_verbosity="-vvv"
+$etcd_instances = 1
+$kube_master_instances = 1
+$num_instances = 5
+$vm_cpus = 16
+$vm_memory = 8192
